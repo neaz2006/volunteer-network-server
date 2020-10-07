8059f32d2877657be5564332249106ac3cc25cf1
neaz2006 <45340875+neaz2006@users.noreply.github.com>
2020-10-07 03:11:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express()
+const app = express();
+require('dotenv').config()
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 const cors = require('cors');
-require('dotenv').config();
+const  ObjectId  = require('mongodb').ObjectID;
+app.use(cors());
 
 
 
@@ -25,29 +29,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
+
   const humanatys = client.db("volunteerdb").collection("charaty");
+  const deletes = client.db("volunteerdb").collection("charaty");
+
   app.post('/addHumanaty', (req, res) => {
     const newHumanaty = req.body;
     humanatys.insertOne(newHumanaty)
       .then(result => {
         console.log(result);
       })
+      
     app.get('/humanatys', (req, res) => {
       humanatys.find({})
         .toArray((err, documents) => {
           res.send(documents);
         })
     })
-    app.delete('/delete/:id', (req, res)=>{
-      ProductCollection.deleteOne({_id:ObjectId(req.params.id)})
-      .then(result=>{
-        res.send(result.insertedCount>0)
-      })
-      
-    })   
+
+    app.delete('/delete/:id', (req, res) => {
+        deletes.deleteOne({ _id: ObjectId(req.params.id) })
+        .then((result) => {
+          res.send(result.deletedCount > 0);
+        })
+    })
 
   })
 });
-app.listen(port);
+app.listen(process.env.PORT || port);
 
 
